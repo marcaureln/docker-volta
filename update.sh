a50c52f6907cdac897cc4adfea73b25b425094f4
Alex N'Guessan <alexmarcaureln@gmail.com>
2024-08-25 19:47:36 +0000

    Fix curl error "(23) Failed writing body"

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ curl -fsSL \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/volta-cli/volta/releases?per_page=1 | \
+  tac | tac | \
   grep -m 1 -o '"tag_name": *"[^"]*"' | \
   grep -o '"[^"]*"$' | \
   tr -d '"' | \
